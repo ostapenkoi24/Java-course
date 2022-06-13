1581f040a5f127ecfd8b8e37d6cbf952e155c64b
Eugene <ostapenkoi24@gmail.com>
2022-06-13 22:52:55 +0300

    - add getter and setter

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -4,6 +4,34 @@ public class Dog {
     private String color;
     private int height;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public Dog(String name, int age, String color, int height) {
         this.name = name;
         this.age = age;
--- src/main/java/Homework_4.java
+++ src/main/java/Homework_4.java
@@ -2,11 +2,27 @@ public class Homework_4 {
     public static void main(String[] args) {
         Dog dog = new Dog("Spike",2,"Black", 50);
         dog.print();
+
         System.out.println("\n");
         Dog dog1 = new Dog(3, 65);
         dog1.print();
+
         System.out.println("\n");
         Dog dog2 = new Dog();
+
+        System.out.println("\n");
+        System.out.println(dog.getAge());
+        System.out.println(dog1.getColor());
+
+        dog1.setAge(5);
+        System.out.println("\n");
+        System.out.println(dog1.getAge());
+
+        System.out.println("\n");
+        System.out.println(dog1.getHeight());
+
+        System.out.println("\n");
+        System.out.println(dog.getName());
     }
 
 
