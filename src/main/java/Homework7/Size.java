0b0ce56ee7ce6f9897c2b460c40616f22c94f174
Eugene <ostapenkoi24@gmail.com>
2022-06-27 15:53:59 +0300

    - homework 7

---
+++ src/main/java/Homework7/Size.java
@@ -0,0 +1,33 @@
+package Homework7;
+
+public enum Size {
+    SMALL(37,46, "S"), MEDIUM(40,48, "M"),
+    LARGE(42,52, "L"), EXTRA_LARGE(46,56, "XL");
+
+    private int width;
+    private int length;
+
+    private String size;
+
+    Size(int width, int length, String size) {
+        this.width = width;
+        this.length = length;
+        this.size = size;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return getSize() + " size: width=" +getWidth() + ",length="+getLength();     }
+}
