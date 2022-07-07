e908f330794a489375a08052531d38fccd2f06dd
Eugene <ostapenkoi24@gmail.com>
2022-07-07 12:16:39 +0300

    - Homework 8 Create method that takes String deletes commas and changes register

---
+++ src/main/java/Homework8/Homework8.java
@@ -0,0 +1,20 @@
+package Homework8;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Homework8 {
+    public static void string(String str) {
+
+        String[] string = str.split(", ");
+        Set<String> set = new TreeSet<>(Arrays.asList(string));
+
+        for (String s : set) {
+            String a = StringUtils.capitalize(s);
+            System.out.println(a);
+        }
+    }
+}
---
+++ src/main/java/Homework8/Test.java
@@ -0,0 +1,8 @@
+package Homework8;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "масло, масло, колбаса, молоко";
+        Homework8.string(str);
+    }
+}
