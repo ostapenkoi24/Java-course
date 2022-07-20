7d903d74ce3ea54dd6abd741d5b533ad095d07f6
Eugene <ostapenkoi24@gmail.com>
2022-07-20 22:28:50 +0300

    - Homework 9 Add file with test

---
+++ src/main/java/gfhfg/New/Test.java
@@ -0,0 +1,33 @@
+package gfhfg.New;
+
+
+import Homework9.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public class Test {
+    static final By SEARCH=By.id("btn-consultation-hero");
+    static final String BASEURL="https://ithillel.ua/courses/qa-automation-kiev";
+
+    public static void main(String[] args) {
+
+        WebDriver driver = DriverFactory.getDriver("CHROME");
+        driver.get(BASEURL);
+        WebElement element = driver.findElement(SEARCH);
+        System.out.println(driver.getTitle());
+        System.out.println(element.isDisplayed());
+        driver.quit();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
