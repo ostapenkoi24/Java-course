19145b845a29ea8f806dd988fbe3d148c316e46a
ostapenkoi24 <60350282+ostapenkoi24@users.noreply.github.com>
2022-09-27 17:10:21 +0300

    Delete src/main/java/Homework8 directory

--- src/main/java/Homework8/Test.java
+++
@@ -1,8 +0,0 @@
-package Homework8;
-
-public class Test {
-    public static void main(String[] args) {
-        String str = "масло, Масло, колбаса, молоко, Колбаса";
-        Homework8.string(str);
-    }
-}
